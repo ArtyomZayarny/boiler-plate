459334a470e2a3e21846a15ac53aafd7933760ec
ArtyomZayarny <artyom.zayarny@gmail.com>
2020-06-04 07:58:30 +0300

    init

--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,15 @@ module.exports = {
                     'css-loader',
                     'sass-loader'
                 ]
-            }
+            },
+            {
+                test: /\.(png|jpe?g|gif)$/i,
+                use: [
+                  {
+                    loader: 'file-loader',
+                  },
+                ],
+              },
         ]
     },
     plugins: [new MiniCssExtractPagin({
